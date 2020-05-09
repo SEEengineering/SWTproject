a3f3719d05f4b6ff538fbd916938bf031716eedf
Anu <aoguntayo2@gmail.com>
2020-05-09 17:19:53 +0100

    Added a sample testcase

--- src/main/java/ie/calc/v1/MassConverter.java
+++ src/main/java/ie/calc/v1/MassConverter.java
@@ -6,11 +6,14 @@ public class MassConverter extends JOptionPane {
     private int selection;
     private String calcInput;
     public MassConverter(String input) {
-        this.calcInput = input;
+        if(input.matches("[a-zA-Z]") || input.matches("$&+,:;=?@#|'<>.^*()%!-"))
+            throw new IllegalArgumentException("Must be numeric character");
+        else
+            this.calcInput = input;
         CalcFunction f = new CalcFunction();
         Object[] option1 = {"Pound", "Stone", "Ounce", "EngNotation", "Quit"};
-        setSelection(JOptionPane.showOptionDialog(null, "Convert " + getCalcInput() +" grams to",
-                "Mass converter", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null,
+        setSelection(showOptionDialog(null, "Convert " + getCalcInput() +" grams to",
+                "Mass converter", YES_NO_CANCEL_OPTION, QUESTION_MESSAGE, null,
                 option1, option1[2]));
 
         switch(getSelection()) {
@@ -34,16 +37,15 @@ public class MassConverter extends JOptionPane {
                         "Inane warning",WARNING_MESSAGE);
         }
     }
-    private int getSelection() {
+    public int getSelection() {
         return selection;
     }
 
-    private void setSelection(int selectedOption) {
+    public void setSelection(int selectedOption) {
         this.selection = selectedOption;
     }
 
-    private double getCalcInput() {
+    public double getCalcInput() {
         return Double.parseDouble(calcInput);
     }
-
 }
--- src/test/java/ie.calc.v1/CalcTest.java
+++ src/test/java/ie.calc.v1/CalcTest.java
@@ -1,4 +1,26 @@
 package ie.calc.v1;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
 
 public class CalcTest {
+    private MassConverter mass;
+
+    @BeforeEach
+    void init(TestInfo testInfo, TestReporter testReporter){
+        testReporter.publishEntry("Testing" + "10" + testInfo.getDisplayName());
+        mass = new MassConverter("10");
+    }
+
+    @DisplayName("Checking MassConverter Constructor for Illegal char")
+    @Test
+    void testConstructorFail() {
+        assertThrows(IllegalArgumentException.class, () -> new MassConverter("a"));
+    }
+
+    @DisplayName("Checking MassConverter Constructor")
+    @Test
+    void testConstructorPass() {
+        assertEquals(10,mass.getCalcInput());
+    }
 }
