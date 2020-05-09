16cf8901af4b8377634f2276bbe6cbea5b97f102
SEEengineering <inoah1656@gmail.com>
2020-05-09 15:56:43 +0100

    Initial commit: Software with test Project

--- src/main/java/ie/calc/v1/CalcRunner.java
+++ src/main/java/ie/calc/v1/CalcRunner.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class CalcRunner {
     public static void main(String[] args) {
         try {
-            UIManager.setLookAndFeel("com.jtattoo.plaf.noire.NoireLookAndFeel");
+            //UIManager.setLookAndFeel("com.jtattoo.plaf.noire.NoireLookAndFeel");
             new CalcConverterGUI().setVisible(true);
         } catch (Exception ex) {
             ex.printStackTrace();
